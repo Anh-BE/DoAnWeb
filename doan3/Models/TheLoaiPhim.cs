5329720b39c594a7e428656d212d94e4f6e8fc0d
Anh-BE <anh874343@gmail.com>
2025-12-09 16:02:10 +0700

    filrtong

---
+++ doan3/Models/TheLoaiPhim.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace doan3.Models
+{
+    public class TheLoaiPhim
+    {
+        DatVeXemPhimEntities1 db = new DatVeXemPhimEntities1();
+        public TheLoaiPhim()
+        {
+
+        }
+        public List<TheLoai> GetTheLoai()
+        {
+            return db.TheLoais.OrderBy(t => t.MaTheLoai).ToList();
+        }
+    }
+}
