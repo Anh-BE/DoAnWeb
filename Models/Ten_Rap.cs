8ce96d369d0c2f51425a007fcfdb15b1a81816af
Anh-BE <anh874343@gmail.com>
2025-12-09 13:32:06 +0700

    Initial commit

---
+++ Models/Ten_Rap.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace doan3.Models
+{
+    
+    public class Ten_Rap
+    {
+        DatVeXemPhimEntities1 db = new DatVeXemPhimEntities1();
+        public Ten_Rap()
+        {
+
+        }
+        public List<Rap_Chieu> GetTenRap()
+        {
+            return db.Rap_Chieu.OrderBy(t => t.MaRap).ToList();
+        }
+    }
+}
---
+++ Models/UserLogin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace doan3.Models
+{
+    [Serializable] 
+    public class UserLogin
+    {
+
+           
+            public string UserName { set; get; }
+            public string GroupID { set; get; }
+    }
+}
